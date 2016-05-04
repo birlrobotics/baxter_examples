993343394c5489e491933950dd92298f6e2e75df
rojas70 <rojas70@gmail.com>
2016-05-04 16:59:49 +0800

    Tried to get scripts to be recognized as python modules. Included them in the src/baxter_examples folder.

--- setup.py
+++ setup.py
@@ -2,9 +2,18 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
+# fetch values from package.xml                                                    
+# setup_args = generate_distutils_setup(
+#     packages=['baxter_examples','baxter_external_devices'],
+#     package_dir={'': 'src'})
+
+# setup(**setup_args)
+
+
 d = generate_distutils_setup()
 d['packages'] = ['baxter_examples', 'baxter_external_devices']
 d['package_dir'] = {'': 'src'}
+
 #d['scripts'] = ['gripper_action_client']
 #d['scripts_dir']={'':'scripts'}
 
